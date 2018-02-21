430816f7751bf8068a24e366ac682dd978f40bce
Karolis Stasaitis <stkarolis@gmail.com>
2018-02-21 23:03:57 +0100

    Better implementation of rla

--- src/cpu.rs
+++ src/cpu.rs
@@ -155,25 +155,19 @@ impl CPU {
 		self.regs.set_flag(Z, false);
 		self.regs.set_flag(N, false);
 		self.regs.set_flag(H, false);
-		self.regs.set_flag(C, (a & (1 << 7)) > 0);
+		self.regs.set_flag(C, (a & (1 << 7)) != 0);
 		self.regs.a = a.rotate_left(1);
 	}
 
 	// TODO: shifting is better here.
 	fn rla(&mut self) {
-		let mut a = self.regs.a;
+		let a = self.regs.a;
 		let c = self.regs.get_flag(C);
 		self.regs.set_flag(Z, false);
 		self.regs.set_flag(N, false);
 		self.regs.set_flag(H, false);
-		self.regs.set_flag(C, (a & (1 << 7)) > 0);
-		a = a.rotate_right(1);
-		if c {
-			a = a | 1;
-		} else {
-			a = a & 0;
-		}
-		self.regs.a = a;
+		self.regs.set_flag(C, (a & (1 << 7)) != 0);
+		self.regs.a = a << 1 | (if c { 1 } else { 0 });
 	}
 
 	fn rrca(&mut self) {
