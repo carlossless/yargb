bbc21001561deffde56b7ca131f30f71dad62aae
Karolis Stasaitis <stkarolis@gmail.com>
2018-02-24 14:18:33 +0100

    Some 8bit loads and adds + subs

--- src/cpu.rs
+++ src/cpu.rs
@@ -38,28 +38,106 @@ macro_rules! dop {
 
 macro_rules! inc_byte {
     ($register:ident) => (
-        |cpu: &mut CPU| { let mut v = cpu.regs.$register; cpu.inc_byte(&mut v); cpu.regs.$register = v; 1 }
+        |cpu: &mut CPU| {
+            let mut v = cpu.regs.$register;
+            cpu.inc_byte(&mut v);
+            cpu.regs.$register = v;
+            1
+        }
     )
 }
 
 macro_rules! dec_byte {
     ($register:ident) => (
-        |cpu: &mut CPU| { let mut v = cpu.regs.$register; cpu.dec_byte(&mut v); cpu.regs.$register = v; 1 }
+        |cpu: &mut CPU| {
+            let mut v = cpu.regs.$register;
+            cpu.dec_byte(&mut v);
+            cpu.regs.$register = v;
+            1
+        }
     )
 }
 
+// TODO: could be replaced by patern matched functions instead of the intermal match
 macro_rules! inc_word {
     ($register:ident) => (
-        |cpu: &mut CPU| { let mut v = cpu.regs.get($register); cpu.inc_word(&mut v); cpu.regs.set($register, v); 2 }
+        |cpu: &mut CPU| {
+            let mut v = cpu.regs.get($register);
+            cpu.inc_word(&mut v);
+            cpu.regs.set($register, v);
+            2
+        }
     )
 }
 
+// TODO: could be replaced by patern matched functions instead of the intermal match
 macro_rules! dec_word {
     ($register:ident) => (
-        |cpu: &mut CPU| { let mut v = cpu.regs.get($register); cpu.dec_word(&mut v); cpu.regs.set($register, v); 2 }
+        |cpu: &mut CPU| {
+            let mut v = cpu.regs.get($register);
+            cpu.dec_word(&mut v);
+            cpu.regs.set($register, v);
+            2
+        }
     )
 }
 
+macro_rules! ld {
+    ($source_register:ident, $target_register:ident) => (
+        |cpu: &mut CPU| {
+            cpu.regs.$source_register = cpu.regs.$target_register;
+            1
+        }
+    )
+}
+
+macro_rules! add {
+    ($source_register:ident, $target_register:ident) => (
+        |cpu: &mut CPU| {
+            let addend = cpu.regs.$source_register;
+            let mut v = cpu.regs.$target_register;
+            v = cpu.add_byte(v, addend);
+            cpu.regs.$target_register = v;
+            1
+        }
+    )
+}
+
+macro_rules! adc {
+    ($source_register:ident, $target_register:ident) => (
+        |cpu: &mut CPU| {
+            let addend = cpu.regs.$source_register;
+            let mut v = cpu.regs.$target_register;
+            v = cpu.add_byte_with_carry(v, addend);
+            cpu.regs.$target_register = v;
+            1
+        }
+    )
+}
+
+macro_rules! sub {
+    ($source_register:ident, $target_register:ident) => (
+        |cpu: &mut CPU| {
+            let addend = cpu.regs.$source_register;
+            let mut v = cpu.regs.$target_register;
+            v = cpu.sub_byte(v, addend);
+            cpu.regs.$target_register = v;
+            1
+        }
+    )
+}
+
+macro_rules! sbc {
+    ($source_register:ident, $target_register:ident) => (
+        |cpu: &mut CPU| {
+            let addend = cpu.regs.$source_register;
+            let mut v = cpu.regs.$target_register;
+            v = cpu.sub_byte_with_carry(v, addend);
+            cpu.regs.$target_register = v;
+            1
+        }
+    )
+}
 
 impl CPU {
     const OPS: &'static [Operation] = &[
@@ -130,6 +208,108 @@ impl CPU {
         dop!("DEC A"              , &dec_byte!(a)), // 0x3D DEC A
         dop!("LD A,{:#2X}"   , u8 , &|cpu: &mut CPU, value| { cpu.regs.a = value; 2 }), // 0x3E LD A,d8
         dop!("CCF"                , &CPU::complement_carry_flag), // 0x3F CCF
+
+        dop!("LD B,B"             , &ld!(b,b)), // 0x40 LD B,B
+        dop!("LD B,C"             , &ld!(b,c)), // 0x41 LD B,C
+        dop!("LD B,D"             , &ld!(b,d)), // 0x42 LD B,D
+        dop!("LD B,E"             , &ld!(b,e)), // 0x43 LD B,E
+        dop!("LD B,H"             , &ld!(b,h)), // 0x44 LD B,H
+        dop!("LD B,L"             , &ld!(b,l)), // 0x45 LD B,L
+        dop!("LD B,(HL)"          , &|cpu: &mut CPU| { cpu.regs.b = cpu.mmu.read_byte(cpu.regs.get_hl()); 2 }), // 0x46 LD B,(HL)
+        dop!("LD B,A"             , &ld!(b,a)), // 0x47 LD B,A
+        dop!("LD C,B"             , &ld!(c,b)), // 0x48 LD C,B
+        dop!("LD C,C"             , &ld!(c,c)), // 0x49 LD C,C
+        dop!("LD C,D"             , &ld!(c,d)), // 0x4A LD C,D
+        dop!("LD C,E"             , &ld!(c,e)), // 0x4B LD C,E
+        dop!("LD C,H"             , &ld!(c,h)), // 0x4C LD C,H
+        dop!("LD C,L"             , &ld!(c,l)), // 0x4D LD C,L
+        dop!("LD C,(HL)"          , &|cpu: &mut CPU| { cpu.regs.c = cpu.mmu.read_byte(cpu.regs.get_hl()); 2 }), // 0x4E LD C,(HL)
+        dop!("LD C,A"             , &ld!(b,a)), // 0x4F LD C,A
+
+        dop!("LD D,B"             , &ld!(d,b)), // 0x50 LD B,B
+        dop!("LD D,C"             , &ld!(d,c)), // 0x51 LD B,C
+        dop!("LD D,D"             , &ld!(d,d)), // 0x52 LD B,D
+        dop!("LD D,E"             , &ld!(d,e)), // 0x53 LD B,E
+        dop!("LD D,H"             , &ld!(d,h)), // 0x54 LD B,H
+        dop!("LD D,L"             , &ld!(d,l)), // 0x55 LD B,L
+        dop!("LD D,(HL)"          , &|cpu: &mut CPU| { cpu.regs.d = cpu.mmu.read_byte(cpu.regs.get_hl()); 2 }), // 0x56 LD D,(HL)
+        dop!("LD D,A"             , &ld!(d,a)), // 0x57 LD B,A
+        dop!("LD E,B"             , &ld!(e,b)), // 0x58 LD C,B
+        dop!("LD E,C"             , &ld!(e,c)), // 0x59 LD C,C
+        dop!("LD E,D"             , &ld!(e,d)), // 0x5A LD C,D
+        dop!("LD E,E"             , &ld!(e,e)), // 0x5B LD C,E
+        dop!("LD E,H"             , &ld!(e,h)), // 0x5C LD C,H
+        dop!("LD E,L"             , &ld!(e,l)), // 0x5D LD C,L
+        dop!("LD E,(HL)"          , &|cpu: &mut CPU| { cpu.regs.e = cpu.mmu.read_byte(cpu.regs.get_hl()); 2 }), // 0x5E LD E,(HL)
+        dop!("LD E,A"             , &ld!(e,a)), // 0x5F LD C,A
+
+        dop!("LD H,B"             , &ld!(h,b)), // 0x60 LD H,B
+        dop!("LD H,C"             , &ld!(h,c)), // 0x61 LD H,C
+        dop!("LD H,D"             , &ld!(h,d)), // 0x62 LD H,D
+        dop!("LD H,E"             , &ld!(h,e)), // 0x63 LD H,E
+        dop!("LD H,H"             , &ld!(h,h)), // 0x64 LD H,H
+        dop!("LD H,L"             , &ld!(h,l)), // 0x65 LD H,L
+        dop!("LD H,(HL)"          , &|cpu: &mut CPU| { cpu.regs.h = cpu.mmu.read_byte(cpu.regs.get_hl()); 2 }), // 0x66 LD H,(HL)
+        dop!("LD H,A"             , &ld!(h,a)), // 0x67 LD H,A
+        dop!("LD L,B"             , &ld!(l,b)), // 0x68 LD L,B
+        dop!("LD L,C"             , &ld!(l,c)), // 0x69 LD L,C
+        dop!("LD L,D"             , &ld!(l,d)), // 0x6A LD L,D
+        dop!("LD L,E"             , &ld!(l,e)), // 0x6B LD L,E
+        dop!("LD L,H"             , &ld!(l,h)), // 0x6C LD L,H
+        dop!("LD L,L"             , &ld!(l,l)), // 0x6D LD L,L
+        dop!("LD L,(HL)"          , &|cpu: &mut CPU| { cpu.regs.l = cpu.mmu.read_byte(cpu.regs.get_hl()); 2 }), // 0x6E LD L,(HL)
+        dop!("LD L,A"             , &ld!(l,a)), // 0x6F LD L,A
+
+        dop!("LD (HL),B"          , &|cpu: &mut CPU| { cpu.mmu.write_byte(cpu.regs.get_hl(), cpu.regs.b); 2 }), // 0x70 LD (HL),B
+        dop!("LD (HL),C"          , &|cpu: &mut CPU| { cpu.mmu.write_byte(cpu.regs.get_hl(), cpu.regs.c); 2 }), // 0x71 LD (HL),C
+        dop!("LD (HL),D"          , &|cpu: &mut CPU| { cpu.mmu.write_byte(cpu.regs.get_hl(), cpu.regs.d); 2 }), // 0x72 LD (HL),D
+        dop!("LD (HL),E"          , &|cpu: &mut CPU| { cpu.mmu.write_byte(cpu.regs.get_hl(), cpu.regs.e); 2 }), // 0x73 LD (HL),E
+        dop!("LD (HL),H"          , &|cpu: &mut CPU| { cpu.mmu.write_byte(cpu.regs.get_hl(), cpu.regs.h); 2 }), // 0x74 LD (HL),H
+        dop!("LD (HL),L"          , &|cpu: &mut CPU| { cpu.mmu.write_byte(cpu.regs.get_hl(), cpu.regs.l); 2 }), // 0x75 LD (HL),L
+        dop!("HALT"               , &CPU::unimplemented), // 0x76 HALT
+        dop!("LD (HL),L"          , &|cpu: &mut CPU| { cpu.mmu.write_byte(cpu.regs.get_hl(), cpu.regs.a); 2 }), // 0x77 LD (HL),L
+        dop!("LD A,B"             , &ld!(a,b)), // 0x78 LD A,B
+        dop!("LD A,C"             , &ld!(a,c)), // 0x79 LD A,C
+        dop!("LD A,D"             , &ld!(a,d)), // 0x7A LD A,D
+        dop!("LD A,E"             , &ld!(a,e)), // 0x7B LD A,E
+        dop!("LD A,H"             , &ld!(a,h)), // 0x7C LD A,H
+        dop!("LD A,L"             , &ld!(a,l)), // 0x7D LD A,L
+        dop!("LD A,(HL)"          , &|cpu: &mut CPU| { cpu.regs.a = cpu.mmu.read_byte(cpu.regs.get_hl()); 2 }), // 0x7E LD A,(HL)
+        dop!("LD A,A"             , &ld!(a,a)), // 0x7F LD A,A
+
+        dop!("ADD A,B"            , &add!(a,b)),
+        dop!("ADD A,C"            , &add!(a,c)),
+        dop!("ADD A,D"            , &add!(a,d)),
+        dop!("ADD A,E"            , &add!(a,e)),
+        dop!("ADD A,H"            , &add!(a,h)),
+        dop!("ADD A,L"            , &add!(a,l)),
+        dop!("ADD A,(HL)"         , &CPU::unimplemented),
+        dop!("ADD A,A"            , &add!(a,a)),
+        dop!("ADC A,B"            , &adc!(a,b)),
+        dop!("ADC A,C"            , &adc!(a,c)),
+        dop!("ADC A,D"            , &adc!(a,d)),
+        dop!("ADC A,E"            , &adc!(a,e)),
+        dop!("ADC A,H"            , &adc!(a,h)),
+        dop!("ADC A,L"            , &adc!(a,l)),
+        dop!("ADC A,(HL)"         , &CPU::unimplemented),
+        dop!("ADC A,A"            , &adc!(a,a)),
+
+        dop!("SUB A,B"            , &sub!(a,b)),
+        dop!("SUB A,C"            , &sub!(a,c)),
+        dop!("SUB A,D"            , &sub!(a,d)),
+        dop!("SUB A,E"            , &sub!(a,e)),
+        dop!("SUB A,H"            , &sub!(a,h)),
+        dop!("SUB A,L"            , &sub!(a,l)),
+        dop!("SUB A,(HL)"         , &CPU::unimplemented),
+        dop!("SUB A,A"            , &sub!(a,a)),
+        dop!("SBC A,B"            , &sbc!(a,b)),
+        dop!("SBC A,C"            , &sbc!(a,c)),
+        dop!("SBC A,D"            , &sbc!(a,d)),
+        dop!("SBC A,E"            , &sbc!(a,e)),
+        dop!("SBC A,H"            , &sbc!(a,h)),
+        dop!("SBC A,L"            , &sbc!(a,l)),
+        dop!("SBC A,(HL)"         , &CPU::unimplemented),
+        dop!("SBC A,A"            , &sbc!(a,a)),
     ];
 
     pub fn new(rom_data: &[u8]) -> CPU {
@@ -212,12 +392,50 @@ impl CPU {
     fn add_word(&mut self, lhs: u16, rhs: u16) -> u16 {
         let result = lhs.wrapping_add(rhs);
         self.regs.set_flag(N, false);
-        self.regs.set_flag(H, (lhs & 0x07FF) + (rhs & 0x07FF) > 0x07FF);
+        self.regs.set_flag(H, ((lhs & 0x07FF) + (rhs & 0x07FF)) > 0x07FF);
         self.regs.set_flag(C, lhs > 0xFFFF - rhs);
         result
     }
 
-    // TODO: using match here is unncessary... can be done more concisely by mutating through conditions
+    fn add_byte(&mut self, lhs: u8, rhs: u8) -> u8 {
+        let result = lhs.wrapping_add(rhs);
+        self.regs.set_flag(Z, result == 0);
+        self.regs.set_flag(N, false);
+        self.regs.set_flag(H, ((lhs & 0x0F) + (rhs & 0x0F)) > 0x0F);
+        self.regs.set_flag(C, lhs > 0xFF - rhs);
+        result
+    }
+
+    fn add_byte_with_carry(&mut self, lhs: u8, rhs: u8) -> u8 {
+        let carry = if self.regs.get_flag(C) { 1 } else { 0 };
+        let result = lhs.wrapping_add(rhs).wrapping_add(carry);
+        self.regs.set_flag(Z, result == 0);
+        self.regs.set_flag(N, false);
+        self.regs.set_flag(H, ((lhs & 0x0F) + (rhs & 0x0F)).wrapping_add(carry) > 0x0F);
+        self.regs.set_flag(C, lhs > 0xFF - rhs - carry);
+        result
+    }
+
+    fn sub_byte(&mut self, lhs: u8, rhs: u8) -> u8 {
+        let result = lhs.wrapping_sub(rhs);
+        self.regs.set_flag(Z, result == 0);
+        self.regs.set_flag(N, true);
+        self.regs.set_flag(H, (lhs & 0x0F) < (rhs & 0x0F));
+        self.regs.set_flag(C, lhs < rhs);
+        result
+    }
+
+    fn sub_byte_with_carry(&mut self, lhs: u8, rhs: u8) -> u8 {
+        let carry = if self.regs.get_flag(C) { 1 } else { 0 };
+        let result = lhs.wrapping_add(rhs).wrapping_add(carry);
+        self.regs.set_flag(Z, result == 0);
+        self.regs.set_flag(N, true);
+        self.regs.set_flag(H, ((lhs & 0x0F) < (rhs & 0x0F).wrapping_add(carry)));
+        self.regs.set_flag(C, (lhs as u16) < (rhs as u16) + (carry as u16));
+        result
+    }
+
+    // TODO: using match here is unnecessary... can be done more concisely by mutating through conditions
     fn decimal_adjust_accumulator(&mut self) -> usize {
         let n = self.regs.get_flag(N);
         let h = self.regs.get_flag(H);
--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,10 @@ use std::fs::File;
 use cpu::CPU;
 
 fn main() {
-    let mut rom_file = File::open("roms/tetris.gb").expect("failed to open tetris, lol");
+    let mut rom_file = File::open("roms/cpu_instrs.gb").expect("failed to open rom, lol");
     let mut rom_data: [u8; 32000] = [0; 32000];
 
-    rom_file.read(&mut rom_data).expect("failed to read tetris, lol");
+    rom_file.read(&mut rom_data).expect("failed to read rom, lol");
 
     let mut cpu = CPU::new(&rom_data);
 
